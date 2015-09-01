2e457c7d5352715132714919b196839a807e3f94
James A Gorrie <james.gorrie@theguardian.com>
2015-09-01 16:21:53 +0100

    reuse mapAll

--- commands.js
+++ commands.js
@@ -1,6 +1,6 @@
 function mapAll(mapperFunc) {
-    return results => {
-        const mapped = results.data.map(mapperFunc);
+    return (results, data) => {
+        const mapped = results.data.map(image => mapperFunc(image, data));
         return Promise.all(mapped);
     };
 }
@@ -17,22 +17,18 @@ export const reindex = mapAll(image => {
     });
 });
 
-export const patchMetadata = (results, data) => {
-    const mapped = results.data.map(image => {
-        const metadataResource = image.data.userMetadata.data.metadata;
-        const patchedData = Object.assign({}, metadataResource.data, data);
-        delete patchedData.keywords;
+export const patchMetadata = mapAll((image, data) => {
+    const metadataResource = image.data.userMetadata.data.metadata;
+    const patchedData = Object.assign({}, metadataResource.data, data);
+    delete patchedData.keywords;
 
-        return metadataResource.put({data: patchedData}).then(() => {
-            console.log(`Patched metadata on ${image.data.id} with:`);
-            console.log(patchedData);
-        }).catch(error => {
-            console.error(`Failed to patch metadata on ${image.data.id}: ${error.message}`);
-        });
+    return metadataResource.put({data: patchedData}).then(() => {
+        console.log(`Patched metadata on ${image.data.id} with:`);
+        console.log(patchedData);
+    }).catch(error => {
+        console.error(`Failed to patch metadata on ${image.data.id}: ${error.message}`);
     });
-
-    return Promise.all(mapped);
-};
+});
 
 // TODO: set metadata or rights
 // TODO: select/map data
