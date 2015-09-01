2366d0c19cbcf0166a4361b2ec9a1d99cd5ed563
James A Gorrie <james.gorrie@theguardian.com>
2015-09-01 16:04:04 +0100

    allow user to patch metadata

--- commands.js
+++ commands.js
@@ -1,8 +1,6 @@
 function mapAll(mapperFunc) {
     return results => {
-        const mapped = results.data.map(image => {
-            console.log(JSON.stringify(image.data));
-        });
+        const mapped = results.data.map(mapperFunc);
         return Promise.all(mapped);
     };
 }
@@ -18,6 +16,23 @@ export const reindex = mapAll(image => {
         console.error(`Failed to reindex ${image.data.id}: ${error.message}`);
     });
 });
+
+export const patchMetadata = (results, data) => {
+    const mapped = results.data.map(image => {
+        const metadataResource = image.data.userMetadata.data.metadata;
+        const patchedData = Object.assign({}, metadataResource.data, data);
+        delete patchedData.keywords;
+
+        return metadataResource.put({data: patchedData}).then(() => {
+            console.log(`Patched metadata on ${image.data.id} with:`);
+            console.log(patchedData);
+        }).catch(error => {
+            console.error(`Failed to patch metadata on ${image.data.id}: ${error.message}`);
+        });
+    });
+
+    return Promise.all(mapped);
+};
 
 // TODO: set metadata or rights
 // TODO: select/map data
