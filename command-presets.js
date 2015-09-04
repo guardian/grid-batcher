ea56087dfbf783a7dabb8ca5cca3c1d3f0a93f19
James A Gorrie <james.gorrie@theguardian.com>
2015-09-04 14:54:29 +0100

    remove test preset

--- command-presets.js
+++ command-presets.js
@@ -2,8 +2,6 @@ const notPicdarFromDiffParams = {free: true, costModelDiff: true, missingIdentif
 
 export const gettyImagesUsageRights = preset('credit:"Getty Images"', notPicdarFromDiffParams);
 
-export const fairfaxUsageRights = preset('credit:"Fairfax Media via Getty Images"', notPicdarFromDiffParams);
-
 function preset(query, params = {}) {
     return {query, params};
 }
